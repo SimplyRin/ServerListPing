0551a55dc55cf004c0cbdf9ee184dcb0c896e629
SimplyRin <simplyrin.dev@gmail.com>
2017-11-19 03:46:03 +0900

    IP アドレスからプレイヤーを取得できるように
    ** 未テスト **

--- src/main/java/net/simplyrin/serverlistping/ServerListPing.java
+++ src/main/java/net/simplyrin/serverlistping/ServerListPing.java
@@ -2,8 +2,10 @@ package net.simplyrin.serverlistping;
 
 import java.net.InetAddress;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,12 +17,27 @@ public class ServerListPing extends JavaPlugin implements Listener {
 	public void onEnable() {
 		plugin = this;
 		plugin.getServer().getPluginManager().registerEvents(this, this);
+		plugin.saveDefaultConfig();
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+
+		plugin.getConfig().set("Address." + player.getAddress().getHostName() + ".UUID", player.getUniqueId().toString());
+		plugin.getConfig().set("Address." + player.getAddress().getHostName() + ".Name", player.getName());
+		plugin.saveConfig();
 	}
 
 	@EventHandler
 	public void onPing(ServerListPingEvent event) {
 		InetAddress address = event.getAddress();
 
-		plugin.getServer().getConsoleSender().sendMessage("[" + address.getHostAddress() + "] <-> InitialHandler has connected");
+		if(plugin.getConfig().isSet("Address." + address.getHostName() + ".Name")) {
+			plugin.getServer().getConsoleSender().sendMessage("[" + address.getHostName() + "|" + plugin.getConfig().getString("Address." + address.getHostName() + ".Name") + "] <-> InitialHandler has connected");
+			return;
+		}
+
+		plugin.getServer().getConsoleSender().sendMessage("[" + address.getHostName() + "] <-> InitialHandler has connected");
 	}
 }
